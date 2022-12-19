f8931b32171310556c80010d2d0d17e6f1616260
mabu2807 <marcel_bulling@gmx.de>
2022-12-19 22:08:13 +0100

    inital commit

---
+++ src/test/java/com/example/azure/springazurecinema/SpringAzureCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azure.springazurecinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
