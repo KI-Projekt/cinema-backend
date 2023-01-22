28ba59f4adf7bb3eaebcb80c0d004c76ac7d0c94
mabu2807 <marcel_bulling@gmx.de>
2023-01-22 18:46:39 +0100

    all Unit Tests

---
+++ src/test/java/de/cinetastisch/backend/model/MovieTest.java
@@ -0,0 +1,152 @@
+package de.cinetastisch.backend.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieTest {
+
+    @Test
+    void getRated() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void canEqual() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getTitle() {
+    }
+
+    @Test
+    void getReleaseYear() {
+    }
+
+    @Test
+    void getPosterImage() {
+    }
+
+    @Test
+    void getRuntime() {
+    }
+
+    @Test
+    void getGenre() {
+    }
+
+    @Test
+    void getDirector() {
+    }
+
+    @Test
+    void getWriter() {
+    }
+
+    @Test
+    void getActors() {
+    }
+
+    @Test
+    void getPlot() {
+    }
+
+    @Test
+    void getTrailer() {
+    }
+
+    @Test
+    void getImdbId() {
+    }
+
+    @Test
+    void getImdbRating() {
+    }
+
+    @Test
+    void getImdbRatingCount() {
+    }
+
+    @Test
+    void getMovieStatus() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void setTitle() {
+    }
+
+    @Test
+    void setReleaseYear() {
+    }
+
+    @Test
+    void setPosterImage() {
+    }
+
+    @Test
+    void setRated() {
+    }
+
+    @Test
+    void setRuntime() {
+    }
+
+    @Test
+    void setGenre() {
+    }
+
+    @Test
+    void setDirector() {
+    }
+
+    @Test
+    void setWriter() {
+    }
+
+    @Test
+    void setActors() {
+    }
+
+    @Test
+    void setPlot() {
+    }
+
+    @Test
+    void setTrailer() {
+    }
+
+    @Test
+    void setImdbId() {
+    }
+
+    @Test
+    void setImdbRating() {
+    }
+
+    @Test
+    void setImdbRatingCount() {
+    }
+
+    @Test
+    void setMovieStatus() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
