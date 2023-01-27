4d206528dabb2c687be30242166522a90aa3580e
930C <93C@pm.me>
2023-01-27 12:18:57 +0100

    Fix JDBC Session

--- src/main/java/de/cinetastisch/backend/ServletInitializer.java
+++ src/main/java/de/cinetastisch/backend/ServletInitializer.java
@@ -1,23 +1,21 @@
 package de.cinetastisch.backend;
 
-import de.cinetastisch.backend.config.Config;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
-//public class ServletInitializer extends SpringBootServletInitializer {
-//
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//		return application.sources(SpringAzureCinemaApplication.class);
-//	}
-//
-//}
+public class ServletInitializer extends SpringBootServletInitializer {
 
-public class ServletInitializer extends AbstractHttpSessionApplicationInitializer {
-
-	public ServletInitializer() {
-		super(Config.class);
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringAzureCinemaApplication.class);
 	}
 
 }
+//
+//public class ServletInitializer extends AbstractHttpSessionApplicationInitializer {
+//
+//	public ServletInitializer() {
+//		super(Config.class);
+//	}
+//
+//}
