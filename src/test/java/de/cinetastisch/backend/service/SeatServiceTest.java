e7dfc894be1ec29557717be4694bae80cab8edfd
mabu2807 <77400133+mabu2807@users.noreply.github.com>
2023-02-09 19:14:19 +0100

    Utest final Nora 01

---
+++ src/test/java/de/cinetastisch/backend/service/SeatServiceTest.java
@@ -0,0 +1,36 @@
+package de.cinetastisch.backend.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SeatServiceTest {
+
+    @Test
+    void getAllSeats() {
+    }
+
+    @Test
+    void getSeat() {
+    }
+
+    @Test
+    void addSeat() {
+    }
+
+    @Test
+    void replaceSeat() {
+    }
+
+    @Test
+    void deleteSeat() {
+    }
+
+    @Test
+    void saveSeat() {
+    }
+
+    @Test
+    void replaceSeats() {
+    }
+}
