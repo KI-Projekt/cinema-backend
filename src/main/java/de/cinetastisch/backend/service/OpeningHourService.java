efb7b64e0249a59cf64746720622b73fe552d4f0
mabu2807 <77400133+mabu2807@users.noreply.github.com>
2023-02-09 16:06:39 +0100

    Utest final 05

--- src/main/java/de/cinetastisch/backend/service/OpeningHourService.java
+++ src/main/java/de/cinetastisch/backend/service/OpeningHourService.java
@@ -13,6 +13,7 @@ import java.util.List;
 @Service
 public class OpeningHourService {
 
+
     OpeningHourRepository openingHourRepository;
 
     public List<OpeningHour> getAllOpeningHours(){
--- src/test/java/de/cinetastisch/backend/service/OpeningHourServiceTest.java
+++ src/test/java/de/cinetastisch/backend/service/OpeningHourServiceTest.java
@@ -1,16 +1,53 @@
 package de.cinetastisch.backend.service;
 
+import de.cinetastisch.backend.dto.request.OpeningHourRequestDto;
+import de.cinetastisch.backend.model.OpeningHour;
+import de.cinetastisch.backend.repository.OpeningHourRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.mockito.Mockito.when;
+@ExtendWith(MockitoExtension.class)
 class OpeningHourServiceTest {
+    @InjectMocks
+    OpeningHourService openingHourService;
+
+    @Mock
+    OpeningHourRepository openingHourRepository;
+
+    LocalTime localTime = LocalTime.of(12,12,12);
+
 
     @Test
     void getAllOpeningHours() {
+        OpeningHour openingHour = new OpeningHour(null, null, null);
+        OpeningHour openingHour2 = new OpeningHour(null, null, null);
+
+        List<OpeningHour> openingHourList = List.of(openingHour, openingHour2);
+
+        when(openingHourRepository.findAll()).thenReturn(openingHourList);
+
+        List<OpeningHour> respone = openingHourService.getAllOpeningHours();
+        assertEquals(openingHourList,respone);
     }
 
     @Test
     void replaceOpeningHour() {
+        OpeningHour oldOpeningHour = new OpeningHour(null, localTime, localTime);
+        OpeningHourRequestDto requestDto =new OpeningHourRequestDto("12:12:12","12:12:12");
+        when(openingHourRepository.getReferenceById((long)1.2)).thenReturn(oldOpeningHour);
+        when(openingHourRepository.save(oldOpeningHour)).thenReturn(oldOpeningHour);
+
+        OpeningHour response = openingHourService.replaceOpeningHour((long)1.2,requestDto);
+
+        assertEquals(oldOpeningHour, response);
     }
 }
